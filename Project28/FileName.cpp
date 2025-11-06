df33bc8af171ad7b59ca6d1cd317dca407258373
m-taylor.13 <m-taylor.13@onu.edu>
2025-11-06 12:23:36 -0500

    finished count_letters

--- Project28/FileName.cpp
+++ Project28/FileName.cpp
@@ -8,22 +8,22 @@ int count_letters(string str);
 
 int main(void) {
 
-	string str = "h...h";
-	cout << "length: " << count_letters(str);
+	string str = "I don't give a Holstein for programmers that can only spell a variable one way. (Dr. Stephany)";
+	cout << "length: " << count_letters(str) << endl;
 
 }
 
 int count_letters(string str) {
 	
 	int letters = 0;
-	char punctuation[] = { ' ', ',', '.', '\'', '\"', '(', ')' };
+	char invalid[] = { ' ', ',', '.', '\'', '\"', '(', ')', '0', '1', '2', '3', '4', '5','6', '7', '8', '9'};
 
 	for (int i = 0; i < str.length(); i++) {
 		
 		bool isLetter = true;
 
-		for (int j = 0; j < 7; j++) {
-			if (str[i] == punctuation[j]) {
+		for (int j = 0; j < sizeof(invalid) / sizeof(invalid[0]); j++) {
+			if (str[i] == invalid[j]) {
 				isLetter = false;
 			}
 		}
